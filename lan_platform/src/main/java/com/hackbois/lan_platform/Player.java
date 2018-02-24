60cb3fcf2a9f3196b400d862a8d15885cf8b6a39
Josh Chen <hsuan-mingchen2021@u.northwestern.edu>
2018-02-24 01:05:50 -0600

    Update Player

--- lan_platform/src/main/java/com/hackbois/lan_platform/Player.java
+++ lan_platform/src/main/java/com/hackbois/lan_platform/Player.java
@@ -4,7 +4,7 @@ package com.hackbois.lan_platform;
  * Created by Fresh Jesh on 2/24/2018.
  */
 
-public class Player {
+public class Player<T> {
     private String name_;
     private Class<T> gamePacket_;
 
@@ -20,7 +20,7 @@ public class Player {
     public Class<T> getGamePacket() {
         return gamePacket_;
     }
-    
+
     public void updateGamePacket(Class<T> gamePacket){
         gamePacket_ = gamePacket;
     }
